b388a7309933bb5ad1580082e9726b4a377a9247
Nikolas Stefano <nz.stefano@gmail.com>
2019-08-30 15:41:23 +0700

    basic
    just the basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <html lang="en">
+      <header className="bg-dark header">
+        <nav className="navbar">
+          <div>
+            <ul className="navbar">
+              <li className="nav-item">Database</li>
+              <li className="nav-item">Guide</li>
+              <li className="nav-item">Calculator</li>
+            </ul>
+            <form className="navbar">
+              <input placeholder="Search" />
+              <button>
+                <i>*Search Icon*</i>
+              </button>
+            </form>
+            <ul className="navbar">
+              <li className="nav-item">
+                <a href="/login/">Log in</a>
+              </li>
+              <li className="nav-item">
+                <a href="/sign-up/">Sign Up</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <div>
+          <ul>
+            <i>Add New Item</i>
+          </ul>
+          <ul>
+            <i>Calculate</i>
+          </ul>
+        </div>
+      </main>
+
+      <footer className="bg-dark">
+        <div>
+          <a href="/contact/">Contact</a> | <a href="/about-us/">About Us</a> |{" "}
+          <a href="privacy-policy/">Privacy Policy</a>
+        </div>
+        <div> ZoraGuide - Ragnarok Mobile Fansite</div>
+      </footer>
+    </html>
   );
 }
 
